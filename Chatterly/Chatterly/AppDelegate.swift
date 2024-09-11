3bfb4edcd535922a045cc40750b644ab1833db2f
Anbu Sekar M <saianbusekar@gmail.com>
2024-09-12 00:53:56 +0530

    dependancies added

--- Chatterly/Chatterly/AppDelegate.swift
+++ Chatterly/Chatterly/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import StreamChat
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let config = ChatClientConfig(apiKey: .init("{{ api_key }}"))
+        ChatClient.init(config: config)
+
         return true
     }
 
